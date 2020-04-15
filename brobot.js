2b1b9e2070d180605fcc4b2f0d1fbe69ef5d6290
Scott Arnette <scott.a.arnette@gmail.com>
2020-04-14 22:56:40 -0400

    Tweaks to messages.

--- brobot.js
+++ brobot.js
@@ -1,14 +1,20 @@
+const fs = require('fs');
 const Discord = require('discord.js');
+const { prefix } = require('./brobot.json');
 
 const client = new Discord.Client();
+client.commands = new Discord.Collection();
 
 client.once('ready', () => {
-    console.log('Ready!');
+    console.info('Ready!');
 });
 
 client.on('message', message => {
-	if (message.content === '!ping') {
-        message.channel.send('Pong.');
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+    const cmdArgs = message.content.slice(prefix.length).split(/ +/);
+    const cmd = cmdArgs.shift().toLowerCase();
+        if (cmd === `ping`) {
+        message.channel.send(`${cmd}\n${cmdArgs}`);
     }
 });
 
